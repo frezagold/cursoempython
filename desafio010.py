50ab2c5934d308fc4445eaa0ce89094ff399205a
Igor Alves <76858556+igorwho08@users.noreply.github.com>
2023-12-21 19:24:18 -0300

    Delete desafio010.py

--- desafio010.py
+++
@@ -1,7 +0,0 @@
-d = int(input('quantos reais voce tem? '))
-di = int(input('quantos dolares voce tem? '))
-r = d * 0.20
-r2 = di * 4.94
-print('o valor em dolares somando para o real é {}'.format(r))
-print('o valor em reais somando em dolares é {}'.format(r2))
-
