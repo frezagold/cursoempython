4eaf2d156cfa585a88a097e07bc2cd1e0f471bba
Igor Alves <76858556+igorwho08@users.noreply.github.com>
2023-12-21 19:23:10 -0300

    Delete desafio003.py

--- desafio003.py
+++
@@ -1,4 +0,0 @@
-numero1 = int(input('fala o primeiro numero mangote:'))
-numero2 = int(input('diga outro manooo:'))
-result = numero1 - numero2
-print('Tu digitou {} e depois digitou {} o seu resultado é: {}'.format(numero1, numero2, result))
