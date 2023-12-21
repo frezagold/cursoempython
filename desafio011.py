01b26b1d505aaf74b9043169e71be33dfd96ce53
Igor Alves <76858556+igorwho08@users.noreply.github.com>
2023-12-21 19:24:25 -0300

    Delete desafio011.py

--- desafio011.py
+++
@@ -1,5 +0,0 @@
-l = int(input('qual a sua largura? '))
-a = int(input('qual é a sua altura? '))
-s1 = l * a
-s2 = s1 / 2
-print('o calculo da area é {} e a quantidade de baldes de tinta é {}'.format(s1, s2))
